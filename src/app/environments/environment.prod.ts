115d2cd3c0a61eeddb665469b763e73f0b1c7538
Gusto Ginting <56625312+gustogg@users.noreply.github.com>
2024-12-15 00:51:36 +0700

    Final
    API Settings

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://1d50-114-79-7-110.ngrok-free.app/api/customers',  // ngrok or your production backend URL
-    listBoxApiUrl: 'https://1d50-114-79-7-110.ngrok-free.app/api/listbox',
-    apiLogin: 'https://1d50-114-79-7-110.ngrok-free.app/api/auth/login',
-    apiRegister: 'https://1d50-114-79-7-110.ngrok-free.app/api/auth/register'
+    apiUrl: 'http:/10.10.13.37:8080/api/customers',  // ngrok or your production backend URL
+    listBoxApiUrl: 'http://10.10.13.37:8080/api/listbox',
+    apiLogin: 'http://10.10.13.37:8080/api/auth/login',
+    apiRegister: 'http://10.10.13.37:8080/api/auth/register'
   };
   
